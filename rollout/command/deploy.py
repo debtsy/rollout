1fe79f9181261679e526d3711ce390ebc5c112f8
kurt spindler <kespindler@gmail.com>
2019-02-11 17:52:42 -0800

    add slack integration

--- rollout/command/deploy.py
+++ rollout/command/deploy.py
@@ -3,6 +3,22 @@ from .util import get_service_name
 import os
 from collections import defaultdict
 import subprocess as sub
+from slackclient import SlackClient
+
+
+def perform_notifications(conf, message):
+    slack_conf = conf.get('notifications', {}).get('slack', None)
+    if slack_conf is None:
+        return
+    sc = SlackClient(
+        client_id=slack_conf['client_id'],
+        client_secret=slack_conf['client_secret'],
+    )
+    sc.api_call(
+        "chat.postMessage",
+        channel=slack_conf['channel'],
+        text=message,
+    )
 
 
 def deploy(args, config):
@@ -12,9 +28,13 @@ def deploy(args, config):
     print('Deploying %s' % service_name)
 
     service_hosts = service.get('hosts')
+
+    deploy_hosts = []
+    log_changes = ''
     for name, host in config.get('hosts').items():
         if name not in service_hosts:
             continue
+        deploy_hosts.append(name)
 
         client = connect.connect(host)
 
@@ -24,12 +44,20 @@ def deploy(args, config):
         directory = deploy.get('script_dir')
 
         print('Running on host %s: %s' % (name, host.get('host')))
+        if not log_changes:
+            client.run('git fetch', directory=directory)
+            log_changes = client.run('git log master..origin/master', directory=directory, capture=True)
         for line in script:
             print(line)
             stdout = client.run(line, directory=directory, capture=True)
             print(stdout)
-
         client.close()
+
+    if not_log_changes:
+        message = 'No changes found.'
+    message = 'Deploying changes to hosts: ' + ','.join(deploy_hosts)
+    message += '\n' + log_changes
+    perform_notifications(config, message)
 
 
 def ssh(args, config):
@@ -46,14 +74,14 @@ def status(args, config):
     hosts = config.get('hosts')
 
     service_status = defaultdict(list)
-    for name, host in hosts.items():
-        client = connect.connect(hosts[name])
+    for hostname, host in hosts.items():
+        client = connect.connect(hosts[hostname])
         for service_name, service in config.get('services').items():
-            if service_name not in service_names:
+            if hostname not in service['hosts']:
                 continue
 
             hash = client.run('git rev-parse --short HEAD', directory=service['deploy']['script_dir'], capture=True)
-            service_status[service_name].append((name, hash))
+            service_status[service_name].append((hostname, hash))
 
     proc = sub.Popen('git rev-parse --short origin/master', stdout=sub.PIPE, shell=True)
     hash, _ = proc.communicate()
--- setup.py
+++ setup.py
@@ -10,24 +10,25 @@ def my_test_suite():
 
 
 setup(name='rollout',
-      version='0.1.0',
-      description='deployment tool',
-      url='http://github.com/kespindler/rollout',
-      author='Kurt Spindler',
-      author_email='kespindler@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      entry_points={
-          'console_scripts': [
-              'rollout=rollout.cli:main',
-          ],
-      },
-      test_suite='setup.my_test_suite',
-      install_requires=[
-          "boto3",
-          "paramiko",
-          "PyYAML",
-          "Jinja2",
-      ],
-      )
+    version='0.1.0',
+    description='deployment tool',
+    url='http://github.com/kespindler/rollout',
+    author='Kurt Spindler',
+    author_email='kespindler@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'rollout=rollout.cli:main',
+        ],
+    },
+    test_suite='setup.my_test_suite',
+    install_requires=[
+        "boto3",
+        "paramiko",
+        "PyYAML",
+        "Jinja2",
+        "slackclient",
+    ],
+)
